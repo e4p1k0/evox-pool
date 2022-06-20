f470bf47d498ad44d111bc004d0e5f75e65f9353
evolution-project <evolutionproject@protonmail.com>
2022-06-20 19:43:59 +0300

    fix

--- pool-frontend/config.js
+++ pool-frontend/config.js
@@ -1,5 +1,5 @@
-var api = "https://testv2.smartcoinpool.net/api";
-let parentCoin = "Zano"
+var api = "https://pool.4privacy.click/api";
+let parentCoin = "evox"
 
 let byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH', 'EH', 'ZH', 'YH']
 
@@ -10,10 +10,10 @@ var facebook = "https://www.facebook.com/<YourPoolFacebook>";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+var blockchainExplorer = "https://chain.evolution-network.org/{symbol}/block/{id}";
+var transactionExplorer = "https://chain.evolution-network.org/{symbol}/transaction/{id}";
+//var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
+//var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
